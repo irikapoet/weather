f90c2380afdc9895c55388ddfafb257149f8471b
Svetlana Fedchenko <irikapoet@mail.ru>
2018-12-24 22:24:08 +0700

    all cities

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function makeVisible(id) {
     var element = document.getElementById(id);
     element.classList.remove('hidden');
+
+
+    localStorage.setItem(id, 1);
 }
 
 function makeInvisible(id) {
@@ -9,4 +12,25 @@ function makeInvisible(id) {
 
 }
 
+var cities = [
+    'novosibirsk',
+    'munich',
+    'moscow',
+    'amsterdam',
+    'vena',
+    'sanfran',
+    'roma',
+    'bangkok',
+    'rio',
+    'toronto',
+    'rejk',
+    'paris',
+    'berlin',
+    'london'
+];
 
+for (var i = 0; i < cities.length; i++) {
+    if (localStorage.getItem(cities[i]) !== null) {
+        makeVisible(cities[i]);
+    }
+}
